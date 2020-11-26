4fade8a19c7232aa44798ab88f2b6ebf0746b53b
kaulshiv <shiv.r.kaul@gmail.com>
2020-11-25 20:53:54 -0500

    changing the getclosestwaypoint alg

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -9,6 +9,7 @@ from cv_bridge import CvBridge,CvBridgeError
 from light_classification.tl_classifier import TLClassifier
 import tf
 import cv2
+import numpy as np
 import yaml
 from scipy.spatial import KDTree
 from helpers import detect_light
@@ -128,8 +129,24 @@ class TLDetector(object):
             int: index of the closest waypoint in self.waypoints
 
         """
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
         closest_idx = self.waypoint_tree.query([x, y], 1)[1]
-        return closest_idx
+
+        # Check if closest is ahead or behind vehicle
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord = self.waypoints_2d[closest_idx-1]
+
+        # Equation for hyperplane through closest_coords
+        cl_vect = np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect = np.array([x, y])
+
+        val = np.dot(cl_vect-prev_vect, pos_vect-cl_vect)
+
+        if val>0:
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        return  closest_idx
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
