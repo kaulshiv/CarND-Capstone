debf8cb5665fe08565b304d393ca866ee89e2beb
kaulshiv <shiv.r.kaul@gmail.com>
2021-01-18 21:48:59 -0500

    tweaking red/green color classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -130,7 +130,7 @@ class TLClassifier(object):
         num_red_pixels = np.sum(red_mask)
         num_green_pixels = np.sum(green_mask)
 
-        if(num_red_pixels<num_green_pixels and num_green_pixels>cropped_img.size*0.2):
+        if(num_red_pixels<num_green_pixels):# and num_green_pixels>cropped_img.size*0.2):
             self.light_prediction = TrafficLight.GREEN
         self.light_prediction = TrafficLight.RED
 
